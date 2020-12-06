2af98016f02085ef5a07c550025e2f1e89cd3603
Octogonapus <firey45@gmail.com>
2020-12-06 12:24:30 -0500

    Clean up KernelDiscoveryForm

--- plugin/src/main/java/com/commonwealthrobotics/bowlerintellij/plugin/KernelDiscoveryForm.java
+++ plugin/src/main/java/com/commonwealthrobotics/bowlerintellij/plugin/KernelDiscoveryForm.java
@@ -23,18 +23,20 @@ import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.ui.components.JBList;
 import java.net.InetAddress;
 import java.nio.file.Paths;
+import java.util.List;
 import javax.annotation.CheckForNull;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.SwingWorker;
 import kotlin.Unit;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-/** Shows the kernels reachable on this network. */
+/** Shows the kernels reachable on this network. Lets you start a kernel managed by the plugin. */
 public class KernelDiscoveryForm {
   private final @NotNull KernelConnectionManager kernelConnectionManager;
   private JBList<Tuple2<String, InetAddress>> kernelsList;
@@ -76,16 +78,8 @@ public class KernelDiscoveryForm {
 
   private void startManagedKernelServer(String kernelName) {
     if (kernelServerFacade == null) {
-      logger.debug("Starting kernel: {}", kernelName);
-      // TODO: Expose the CLI path as a setting
-      kernelServerFacade =
-          new KernelServerFacade(
-              kernelName,
-              Paths.get(
-                  "/home/salmon/Documents/bowler-kernel/cli/build/install/cli/bin/bowler-kernel"));
-      kernelServerFacade.ensureStarted();
-      delayedUpdateKernelsListModel(2000); // Update to show the new kernel
-      setNewKernelServerUIState();
+      disableManagedKernelServerUI();
+      new StartManagedKernelServer(kernelName).execute();
     } else {
       logger.debug(
           "Not starting a new kernel server with name {} because one is already running.",
@@ -97,11 +91,8 @@ public class KernelDiscoveryForm {
     if (kernelServerFacade == null) {
       logger.debug("Not stopping the kernel server because there is not one running.");
     } else {
-      logger.debug("Stopping kernel");
-      kernelServerFacade.ensureStopped();
-      delayedUpdateKernelsListModel(1000); // Update to stop showing the kernel
-      kernelServerFacade = null;
-      setNewKernelServerUIState();
+      disableManagedKernelServerUI();
+      new StopManagedKernelServer();
     }
   }
 
@@ -111,40 +102,13 @@ public class KernelDiscoveryForm {
       return; // Nothing to connect to
     }
 
-    logger.debug(
-        "Connecting to kernel {}:{}",
-        selectedKernel.getA(),
-        selectedKernel.getB().getHostAddress());
-
-    // TODO: Expose these arguments
-    final var port =
-        NameClient.INSTANCE
-            .getGrpcPort(selectedKernel.getB(), NameServer.defaultPort, 1000, 10)
-            .attempt()
-            .unsafeRunSync();
-
-    port.fold(
-        ex -> {
-          logger.debug("Failed to get the kernel port.", ex);
-          currentlyConnectedKernelLabel.setText("Error connecting.");
-          return null;
-        },
-        p -> {
-          logger.debug(
-              "Connected to kernel {}:{} on port {}",
-              selectedKernel.getA(),
-              selectedKernel.getB().getHostAddress(),
-              p);
-          kernelConnectionManager.connect(selectedKernel.getB(), p);
-          currentlyConnectedKernelLabel.setText(selectedKernel.getB().getHostAddress() + ":" + p);
-          setSelectedKernelUIState();
-          return null;
-        });
+    disableSelectedKernelUI();
+    new ConnectToKernel(selectedKernel).execute();
   }
 
   private void disconnectFromKernel() {
-    kernelConnectionManager.disconnect();
-    setSelectedKernelUIState();
+    disableSelectedKernelUI();
+    new DisconnectFromKernel().execute();
   }
 
   private Unit updateConnectedKernel(@CheckForNull Tuple2<? extends InetAddress, Integer> kernel) {
@@ -165,17 +129,17 @@ public class KernelDiscoveryForm {
     kernelsListModel.addAll(kernels);
   }
 
-  private void delayedUpdateKernelsListModel(long delayMs) {
-    new Thread(
-            () -> {
-              try {
-                Thread.sleep(delayMs);
-              } catch (InterruptedException e) {
-                e.printStackTrace();
-              }
-              updateKernelsListModel();
-            })
-        .start();
+  private void setManagedKernelServerUIState() {
+    var kernelServerStarted = kernelServerFacade != null && kernelServerFacade.isStarted();
+    startManagedKernelServerButton.setEnabled(!kernelServerStarted);
+    stopManagedKernelServerButton.setEnabled(kernelServerStarted);
+    managedKernelServerNameField.setEnabled(!kernelServerStarted);
+  }
+
+  private void disableManagedKernelServerUI() {
+    startManagedKernelServerButton.setEnabled(false);
+    stopManagedKernelServerButton.setEnabled(false);
+    managedKernelServerNameField.setEnabled(false);
   }
 
   private void setSelectedKernelUIState() {
@@ -183,10 +147,153 @@ public class KernelDiscoveryForm {
     disconnectFromCurrentKernelButton.setEnabled(kernelConnectionManager.isConnected());
   }
 
-  private void setNewKernelServerUIState() {
-    var kernelServerStarted = kernelServerFacade != null && kernelServerFacade.isStarted();
-    startManagedKernelServerButton.setEnabled(!kernelServerStarted);
-    stopManagedKernelServerButton.setEnabled(kernelServerStarted);
-    managedKernelServerNameField.setEnabled(!kernelServerStarted);
+  private void disableSelectedKernelUI() {
+    connectToSelectedKernelButton.setEnabled(false);
+    disconnectFromCurrentKernelButton.setEnabled(false);
+  }
+
+  private class UpdateKernelsList
+      extends SwingWorker<List<Tuple2<String, InetAddress>>, Tuple2<String, InetAddress>> {
+
+    private final long delayMs;
+
+    public UpdateKernelsList(long delayMs) {
+      this.delayMs = delayMs;
+    }
+
+    @Override
+    protected List<Tuple2<String, InetAddress>> doInBackground() throws Exception {
+      Thread.sleep(delayMs);
+      // TODO: Expose these arguments
+      return NameClient.INSTANCE.scan(
+          NameServer.Companion.getDefaultMulticastGroup(), NameServer.defaultPort, 1000);
+    }
+
+    @Override
+    protected void done() {
+      kernelsListModel.clear();
+      try {
+        kernelsListModel.addAll(get());
+      } catch (Exception ignore) {
+      }
+    }
+  }
+
+  private class StartManagedKernelServer extends SwingWorker<Void, Void> {
+    private final String kernelName;
+
+    public StartManagedKernelServer(String kernelName) {
+      this.kernelName = kernelName;
+    }
+
+    @Override
+    protected Void doInBackground() {
+      logger.debug("Starting a new managed kernel with name {}", kernelName);
+
+      // TODO: Expose the CLI path as a setting
+      kernelServerFacade =
+          new KernelServerFacade(
+              kernelName,
+              Paths.get(
+                  "/home/salmon/Documents/bowler-kernel/cli/build/install/cli/bin/bowler-kernel"));
+      kernelServerFacade.ensureStarted();
+
+      var updateKernelsWorker = new UpdateKernelsList(2000);
+      updateKernelsWorker.execute();
+
+      return null;
+    }
+
+    @Override
+    protected void done() {
+      setManagedKernelServerUIState();
+    }
+  }
+
+  private class StopManagedKernelServer extends SwingWorker<Void, Void> {
+
+    @Override
+    protected Void doInBackground() {
+      logger.debug("Stopping managed kernel");
+      kernelServerFacade.ensureStopped();
+
+      var updateKernelsWorker = new UpdateKernelsList(1000);
+      updateKernelsWorker.execute();
+
+      kernelServerFacade = null;
+
+      return null;
+    }
+
+    @Override
+    protected void done() {
+      setManagedKernelServerUIState();
+    }
+  }
+
+  private class ConnectToKernel extends SwingWorker<Tuple2<InetAddress, Integer>, Void> {
+    private final Tuple2<String, InetAddress> selectedKernel;
+
+    public ConnectToKernel(Tuple2<String, InetAddress> selectedKernel) {
+      this.selectedKernel = selectedKernel;
+    }
+
+    @Override
+    protected Tuple2<InetAddress, Integer> doInBackground() {
+      logger.debug(
+          "Connecting to kernel {}:{}",
+          selectedKernel.getA(),
+          selectedKernel.getB().getHostAddress());
+
+      // TODO: Expose these arguments
+      final var port =
+          NameClient.INSTANCE
+              .getGrpcPort(selectedKernel.getB(), NameServer.defaultPort, 1000, 10)
+              .attempt()
+              .unsafeRunSync();
+
+      return port.fold(
+          ex -> {
+            logger.debug("Failed to get the kernel port.", ex);
+            return null;
+          },
+          p -> {
+            logger.debug(
+                "Connected to kernel {}:{} on port {}",
+                selectedKernel.getA(),
+                selectedKernel.getB().getHostAddress(),
+                p);
+            kernelConnectionManager.connect(selectedKernel.getB(), p);
+            return new Tuple2<>(selectedKernel.getB(), p);
+          });
+    }
+
+    @Override
+    protected void done() {
+      try {
+        var result = get();
+        if (result == null) {
+          currentlyConnectedKernelLabel.setText("Error connecting.");
+        } else {
+          currentlyConnectedKernelLabel.setText(
+              result.getA().getHostAddress() + ":" + result.getB());
+          setSelectedKernelUIState();
+        }
+      } catch (Exception ignore) {
+      }
+    }
+  }
+
+  private class DisconnectFromKernel extends SwingWorker<Void, Void> {
+    @Override
+    protected Void doInBackground() {
+      kernelConnectionManager.disconnect();
+      return null;
+    }
+
+    @Override
+    protected void done() {
+      setSelectedKernelUIState();
+    }
   }
 }
